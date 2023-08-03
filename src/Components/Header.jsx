20cb9545237dd68a9abc59a65551f7125f54de50
It <ithiengkrub@gmail.com>
2023-08-04 01:58:52 +0700

    feat: add 5 section on skill checkpoint day 1

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1 class='text-3xl'>My Profile</h1>
+    </div>
+  );
+};
+
+export default Header;
